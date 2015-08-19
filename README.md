ff3cd1102c2ef2aedada1bb0cc4b58f72346e77f
Benjamin Eidelman <beneidel@gmail.com>
2015-08-19 14:46:12 -0300

    help

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-markdown-diagram
+Markdown-Diagram
 ================
 
-generate diagrams from markdown documents
+How do you build a diagram for the structure in a markdown document?  [Just read this one](http://benjamine.github.io/markdown-diagram/page?url=./help.md).
--- src/generator.js
+++ src/generator.js
@@ -37,62 +37,73 @@ function generate(md, options) {
 
   var dot = ['digraph {'];
 
-  var title = '';
+  var title = opt.title || '';
 
   var items;
 
-  var parentElement = $(opt.selectors.parent).first();
-  var parentTagName = parentElement.get(0).tagName;
-  if (/h\d/i.test(parentTagName)) {
-    // parent is a title, look for next sibling elements
-    if (!opt.selectors.item) {
-      opt.selectors.item = parentTagName.slice(0, 1) +
-        (parseInt(parentTagName.slice(1, 2)) + 1);
-    }
-    items = parentElement.nextUntil(parentTagName);
-    items = items.find(opt.selectors.item).addBack().filter(opt.selectors.item);
-  } else if (/[uo]l/i.test(parentTagName)) {
-    // parent is a list, look for list items
-    if (!opt.selectors.item) {
-      opt.selectors.item = 'li';
-    }
+  var parentSelector = opt.selectors.parent;
+  if (/^[\w\d\-]+$/.test(parentSelector)) {
+    parentSelector += ',#' + parentSelector;
   }
-  items = items || parentElement.find(opt.selectors.item || 'h2');
-  title = opt.title || parentElement.text().trim();
+
+  var parentElement = $(parentSelector).first();
+  if (parentElement.length) {
+    var parentTagName = parentElement.get(0).tagName;
+    if (/h\d/i.test(parentTagName)) {
+      // parent is a title, look for next sibling elements
+      if (!opt.selectors.item) {
+        opt.selectors.item = parentTagName.slice(0, 1) +
+          (parseInt(parentTagName.slice(1, 2)) + 1);
+      }
+      items = parentElement.nextUntil(parentTagName);
+      items = items.find(opt.selectors.item).addBack().filter(opt.selectors.item);
+    } else if (/[uo]l/i.test(parentTagName)) {
+      // parent is a list, look for list items
+      if (!opt.selectors.item) {
+        opt.selectors.item = 'li';
+      }
+    }
+    items = items || parentElement.find(opt.selectors.item || 'h2');
+    title = title || parentElement.text().trim();
+  } else {
+    title = 'parent element not found';
+  }
 
   opt.selectors.item = opt.selectors.item || 'h2';
 
-  title = title || opt.title;
   if (title) {
     title = title.replace(/¶/g, '').trim();
     dot.push('labelloc="t";');
     dot.push('label="' + title + '";');
   }
 
-  var itemsByUrl = {};
-  items.each(function(index, element) {
-    var item = $(element);
-    itemsByUrl[item.find('a').attr('href')] = item;
-  });
   function itemTitle(item) {
      return item.text().replace(/[^\w\d -_,;\.]/ig, '').trim();
   }
-  items.each(function(index, element) {
-    var item = $(element);
-    var itemContent = item.nextUntil(opt.selectors.item);
-    var links = itemContent.find(opt.selectors.itemLinks);
-    dot.push('  "' + itemTitle(item) + '" [shape=box, URL="' + item.find('a').attr('href') + '"];');
-    links.each(function(index, linkElement) {
-      var link = $(linkElement);
-      var targetItem = itemsByUrl[link.attr('href')];
-      if (targetItem) {
-        var relationText = link.prev().text().trim();
-        var label = relationText ? ' [label="' + relationText + '"]' : '';
-        dot.push('  "' + itemTitle(item) + '" -> "' + itemTitle(targetItem) +
-          '"' + label + ';');
-      }
+
+  if (items && items.length) {
+    var itemsByUrl = {};
+    items.each(function(index, element) {
+      var item = $(element);
+      itemsByUrl[item.find('a').attr('href')] = item;
     });
-  });
+    items.each(function(index, element) {
+      var item = $(element);
+      var itemContent = item.nextUntil(opt.selectors.item);
+      var links = itemContent.find(opt.selectors.itemLinks);
+      dot.push('  "' + itemTitle(item) + '" [shape=box, URL="' + item.find('a').attr('href') + '"];');
+      links.each(function(index, linkElement) {
+        var link = $(linkElement);
+        var targetItem = itemsByUrl[link.attr('href')];
+        if (targetItem) {
+          var relationText = link.prev().text().trim();
+          var label = relationText ? ' [label="' + relationText + '"]' : '';
+          dot.push('  "' + itemTitle(item) + '" -> "' + itemTitle(targetItem) +
+            '"' + label + ';');
+        }
+      });
+    });
+  }
 
   dot.push('}');
   dot = dot.join('\n');
